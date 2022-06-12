25ce82b10d062ad1687bfc31523eefd63749a00c
Oregano <kankowska.katarzyna@gmail.com>
2022-06-12 17:54:19 +0200

    Add solution to second exercise from Iterators chapter
    * App takes 5 words from user and asks for a string to remove (if element of list contains the string, it will be removed). Code was fixed to iterate correctly with Iterator

---
+++ src/Iteratory2/WordsRemover.java
@@ -0,0 +1,26 @@
+package Iteratory2;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.Scanner;
+
+class WordsRemover {
+    public static void main(String[] args) {
+        ArrayList<String> words = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Podaj wyraz nr " + (i + 1));
+            words.add(scanner.nextLine());
+        }
+
+        System.out.println("Podaj tekst do odfiltrowania:");
+        String filterWord = scanner.nextLine();
+        Iterator<String> wordsIterator = words.iterator();
+        while(wordsIterator.hasNext()){
+            if(wordsIterator.next().toLowerCase().contains(filterWord.toLowerCase()))
+                wordsIterator.remove();
+        }
+        System.out.println(words);
+    }
+}

06e650148265c9f05cc20e03abc609ae9992e2de
Oregano <kankowska.katarzyna@gmail.com>
2022-06-17 19:26:31 +0200

    Add solution to first exercise from References to methods chapter
    *Use reference to static method compareToIgnoreCase from String class instead of comparator

---
+++ src/ReferencesToMethods/SimpleNamesComparator.java
@@ -0,0 +1,19 @@
+package ReferencesToMethods;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SimpleNamesComparator {
+
+    public static void main(String[] args) {
+        List<String>  names = Arrays.asList("Zyta", "Bogusław", "Aldona", "Bogdan", "Jadwiga");
+        System.out.println("Przed sortowaniem");
+        names.forEach(System.out::println);
+        names.sort(String::compareToIgnoreCase);
+        System.out.println("Po sortowaniu");
+        names.forEach(System.out::println);
+    }
+
+
+
+}

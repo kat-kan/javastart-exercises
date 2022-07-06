8ae2ca36698ac1dcd3cf27850a247acf755c9619
Oregano <kankowska.katarzyna@gmail.com>
2022-07-06 23:58:43 +0200

    Refactor ContactApp class to use ifPresentOrElse
    TODO refactor in progress

--- src/Optional/ContactApp.java
+++ src/Optional/ContactApp.java
@@ -4,14 +4,24 @@ import java.util.Optional;
 import java.util.Scanner;
 
 public class ContactApp {
+    static final String FILE_NAME = "resources/contacts.csv";
+
     public static void main(String[] args) {
-        Optional<ContactManager> contactManager = ContactReader.readFile("resources/contacts.csv");
+//        Optional<ContactManager> contactManager = ContactReader.readFile(FILE_NAME);
+        ContactReader.readFile(FILE_NAME).ifPresentOrElse(ContactApp::readEmailFromUserAndShowContactInfo, ContactApp::fileNotFoundInfo);
+    }
+
+    private static void readEmailFromUserAndShowContactInfo(ContactManager contactManager) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj adres email do wyszukania kontaktu:");
         String email = scanner.nextLine();
-        contactManager.ifPresent(c->c.findByEmail(email));
-        Optional<Contact> contactByEmail = contactManager.flatMap(manager -> manager.findByEmail(email));
-        System.out.println("Kontakt o wskazanym adresie email:");
-        contactByEmail.ifPresent(c-> System.out.println(c.getShortInfo()));
+        /*contactManager.ifPresent(c->c.findByEmail(email));
+        Optional<Contact> contactByEmail = contactManager.flatMap(manager -> manager.findByEmail(email));*/
+        /*System.out.println("Kontakt o wskazanym adresie email:");
+        contactByEmail.ifPresent(c-> System.out.println(c.getShortInfo()));*/
+    }
+
+    private static void fileNotFoundInfo(){
+        System.out.println("nie znaleziono pliku");
     }
 }

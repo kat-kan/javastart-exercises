1ebe32560598a3b1c361b88349e591961c5bf9a5
Oregano <kankowska.katarzyna@gmail.com>
2022-07-07 12:14:43 +0200

    Refactor ContactApp class to use ifPresentOrElse part 2
    Final changes in ContactApp class. Add new methods and handle Optionals with ifPresentOrElse

--- src/Optional/ContactApp.java
+++ src/Optional/ContactApp.java
@@ -1,27 +1,33 @@
 package Optional;
 
-import java.util.Optional;
 import java.util.Scanner;
 
 public class ContactApp {
     static final String FILE_NAME = "resources/contacts.csv";
 
     public static void main(String[] args) {
-//        Optional<ContactManager> contactManager = ContactReader.readFile(FILE_NAME);
-        ContactReader.readFile(FILE_NAME).ifPresentOrElse(ContactApp::readEmailFromUserAndShowContactInfo, ContactApp::fileNotFoundInfo);
+        ContactReader.readFile(FILE_NAME)
+                .ifPresentOrElse(ContactApp::readEmailFromUserAndShowContactInfo, ContactApp::fileNotFoundInfo);
     }
 
     private static void readEmailFromUserAndShowContactInfo(ContactManager contactManager) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj adres email do wyszukania kontaktu:");
         String email = scanner.nextLine();
-        /*contactManager.ifPresent(c->c.findByEmail(email));
-        Optional<Contact> contactByEmail = contactManager.flatMap(manager -> manager.findByEmail(email));*/
-        /*System.out.println("Kontakt o wskazanym adresie email:");
-        contactByEmail.ifPresent(c-> System.out.println(c.getShortInfo()));*/
+        contactManager.findByEmail(email)
+                .ifPresentOrElse(ContactApp::printContantInfo, ContactApp::contactNotFoundInfo);
     }
 
-    private static void fileNotFoundInfo(){
+    private static void printContantInfo(Contact contact) {
+        System.out.println("Kontakt o wskazanym adresie email:");
+        System.out.println(contact.getShortInfo());
+    }
+
+    private static void fileNotFoundInfo() {
         System.out.println("nie znaleziono pliku");
+    }
+
+    private static void contactNotFoundInfo() {
+        System.out.println("nie znaleziono kontaktu");
     }
 }

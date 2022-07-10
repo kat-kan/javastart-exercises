b0de1c53bf6765da2c2805d8d6de8f841442ca19
Oregano <kankowska.katarzyna@gmail.com>
2022-07-10 22:40:07 +0200

    Final solution for exercise from String format chapter

--- src/StringFormat/Loader.java
+++ src/StringFormat/Loader.java
@@ -1,24 +1,38 @@
 package StringFormat;
 
 public class Loader {
+    public static final int STEP_MILLIS = 200;
+    public static final double STEP_PERCENT = 0.5;
+    public static final int PERCENT_MAX_VALUE = 100;
+
+    static double loadingPercent = 0.0;
+    static String halfPercentIndicator;
+    static String progressBar;
+
     public static void main(String[] args) {
-        double loadingPercent = 0.0;
-        while (loadingPercent <= 100){
+        StringBuilder builder = new StringBuilder();
+        while (loadingPercent < PERCENT_MAX_VALUE) {
             try {
-                Thread.sleep(200);
-                loadingPercent+= 0.5;
-                String halfPercentSign = loadingPercent % 1.0 == 0 ? "/": "\\";
-                /*String tenPercentSign = " ";
-                StringBuilder builder = new StringBuilder();
-                if (loadingPercent%10.0==0){
-                    builder.append(tenPercentSign).append("#");
-                }
-                tenPercentSign = builder.toString();*/
-                System.out.printf("Wczytywanie: %.2f %% %s \r", loadingPercent, halfPercentSign);
+                Thread.sleep(STEP_MILLIS);
+                loadingPercent += STEP_PERCENT;
+                halfPercentIndicator = getHalfPercentIndicator(loadingPercent);
+                progressBar = getProgressBar(loadingPercent, builder);
+                System.out.printf("Wczytywanie: %5.2f%% %s (%-10s) \r", loadingPercent, halfPercentIndicator, progressBar);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+    }
 
+    private static String getHalfPercentIndicator(double loadingPercent) {
+        return loadingPercent % 1.0 == 0 ? "/" : "\\";
+    }
+
+    private static String getProgressBar(double loadingPercent, StringBuilder builder) {
+        String progressBarSign = "#";
+        if (loadingPercent % 10.0 == 0) {
+            builder.append(progressBarSign);
+        }
+        return builder.toString();
     }
 }
